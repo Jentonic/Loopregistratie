2fcf3ad4db462c7ee36b16e7722e70e68c21d076
Jentonic <jennebaeten8@hotmail.com>
2018-10-16 20:03:17 +0200

    Initial commit

---
+++ Loopregistratie/MainPage.xaml.cs
@@ -0,0 +1,121 @@
+﻿using Loopregistratie.Views.Home;
+using Loopregistratie.Views.Queue;
+using Loopregistratie.Views.Runners;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Loopregistratie
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private Type currentPage;
+
+        private readonly IList<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>
+        {
+            ("home", typeof(HomePage)),
+            ("runners", typeof(RunnersPage)),
+            ("queue", typeof(QueuePage)),
+        };
+
+        #region NavigationView event handlers
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            // set the initial SelectedItem
+            foreach (NavigationViewItemBase item in NavView.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "home")
+                {
+                    NavView.SelectedItem = item;
+                    break;
+                }
+            }
+            ContentFrame.Navigated += On_Navigated;
+            NavView_Navigate("home");
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.InvokedItem == null)
+                return;
+
+            //Uncomment if you use a settingspage
+            if (args.IsSettingsInvoked)
+            {
+                //ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                // Get Tag from args.InvokedItem
+                var navItemTag = NavView.MenuItems
+                    .OfType<NavigationViewItem>()
+                    .First(i => args.InvokedItem.Equals(i.Content))
+                    .Tag.ToString();
+
+                NavView_Navigate(navItemTag);
+            }
+
+        }
+
+        private void NavView_Navigate(string navItemTag)
+        {
+            var item = _pages.First(p => p.Tag.Equals(navItemTag));
+            if (currentPage == item.Page)
+                return;
+            ContentFrame.Navigate(item.Page);
+
+            currentPage = item.Page;
+        }
+
+        private void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            // If I use a settingspage
+            //if (ContentFrame.SourcePageType == typeof(SettingsPage))
+            //{
+            //    NavView.SelectedItem = (NavigationViewItem)NavView.SettingsItem;
+            //}
+            //else
+            //{
+            //    var item = _pages.First(p => p.Page == e.SourcePageType);
+
+            //    NavView.SelectedItem = NavView.MenuItems
+            //        .OfType<NavigationViewItem>()
+            //        .First(n => n.Tag.Equals(item.Tag));
+            //}
+
+            //disable this section if I use a settingspage
+            var item = _pages.First(p => p.Page == e.SourcePageType);
+
+            NavView.SelectedItem = NavView.MenuItems
+                .OfType<NavigationViewItem>()
+                .First(n => n.Tag.Equals(item.Tag));
+        }
+        #endregion
+    }
+}
